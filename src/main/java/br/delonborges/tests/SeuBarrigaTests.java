47e568138ebbb3d44ff1d5a55d68e9b4dc72b57b
Delon Borges <delon.borges@icloud.com>
2021-07-02 17:41:17 -0300

    Add account creation test

--- src/main/java/br/delonborges/tests/SeuBarrigaTests.java
+++ src/main/java/br/delonborges/tests/SeuBarrigaTests.java
@@ -3,6 +3,9 @@ package br.delonborges.tests;
 import br.delonborges.common.BaseTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 
 public class SeuBarrigaTests extends BaseTest {
@@ -14,5 +17,29 @@ public class SeuBarrigaTests extends BaseTest {
             .get("/contas")
         .then()
             .statusCode(401);
+    }
+
+    @Test
+    public void deveIncluirContaComSucesso() {
+        Map<String, String> login = new HashMap<>();
+        login.put("email", "delon@borges.com");
+        login.put("senha", "delonborges");
+
+        String token = given()
+            .body(login)
+        .when()
+            .post("/signin")
+        .then()
+            .statusCode(200)
+            .extract()
+                .path("token");
+
+        given()
+            .header("Authorization", "JWT " + token)
+            .body("{\"nome\": \"Conta do Delon\"}\n")
+        .when()
+            .post("/contas")
+        .then()
+            .statusCode(201);
     }
 }
