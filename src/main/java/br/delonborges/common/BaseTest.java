d567b8e5b91cc825cfcfdd097bead18a365db946
Delon Borges <delon.borges@icloud.com>
2021-07-02 11:04:37 -0300

    Add common files

---
+++ src/main/java/br/delonborges/common/BaseTest.java
@@ -0,0 +1,27 @@
+package br.delonborges.common;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest implements Constants {
+    @BeforeAll
+    public static void SetUp() {
+        RestAssured.baseURI = APP_BASE_URL;
+        RestAssured.port = APP_PORT;
+        RestAssured.basePath = APP_BASE_PATH;
+
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setContentType(APP_CONTENT_TYPE);
+        RestAssured.requestSpecification = requestSpecBuilder.build();
+
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+        responseSpecBuilder.expectResponseTime(Matchers.lessThan(MAX_TIMEOUT));
+        RestAssured.responseSpecification = responseSpecBuilder.build();
+
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+}
